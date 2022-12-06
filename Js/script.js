5b69bb0f457ac9b4bb8114b45d431f2a51e872f8
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-06 16:29:15 +0100

    inizio collegamento al bottone per creazione della griglia

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+const bottone = document.getElementById('bottone-play');
+
+bottone.addEventListener('click', function(){
+    function creaQuadrato(){
+        const quadrato = document.createElement('div');
+        quadrato.classList.add('quadrato');
+    }
+})
