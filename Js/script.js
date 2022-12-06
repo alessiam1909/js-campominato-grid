5148c2011c346f55ff13ee508f05b45fe90afb8e
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-06 22:01:01 +0100

    conclusione esercizio

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,44 @@
 const bottone = document.getElementById('bottone-play');
 
+
+function creaQuadrato(numero){
+
+    const quadrato = document.createElement('div');
+
+    quadrato.classList.add('quadrato');
+
+    quadrato.innerText = ++numero;
+
+    return quadrato; 
+}
+
 bottone.addEventListener('click', function(){
-    function creaQuadrato(){
-        const quadrato = document.createElement('div');
-        quadrato.classList.add('quadrato');
+
+    let grid = document.getElementById('grid');
+    
+    grid.innerHTML = "";
+
+    for (let i = 0; i < 100; i++){
+           
+        const quadratoDinamico = creaQuadrato(i);
+
+        quadratoDinamico.addEventListener('click', function(){
+            this.classList.add('clicked');
+            console.log(this.textContent);
+        })
+                
+        grid.appendChild(quadratoDinamico);
+
+
+    
     }
+
+      
+
+    
+
+
+
+    
+    
 })
