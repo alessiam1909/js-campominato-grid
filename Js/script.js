7be875fe18bf4b78484e9988e1c99fea795ba478
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-07 13:02:10 +0100

    creazione bonus esercizio con 3 difficoltà

--- Js/script.js
+++ Js/script.js
@@ -1,35 +1,113 @@
 const bottone = document.getElementById('bottone-play');
 
+function createNewGame(){
+    let difficulty = parseInt(document.getElementById('level').value) ;
+    console.log(difficulty);
 
-function creaQuadrato(numero){
+    let cellsNumber;
+    let cellsPerRow;
 
-    const quadrato = document.createElement('div');
+    switch (difficulty){
+        case 1:
+            cellsNumber = 100;
+            cellsPerRow = 10;
+            break;
+        case 2:
+            cellsNumber = 81;
+            cellsPerRow = 9;
+            break;
+        case 3:
+            cellsNumber = 49;
+            cellsPerRow = 7;
+            break;
+        default:
+            cellsNumber = 100;
+            cellsPerRow = 10;
+            break;  
+    }
 
-    quadrato.classList.add('quadrato');
-
-    quadrato.innerText = ++numero;
-
-    return quadrato; 
+    generateGameGrid(cellsNumber, cellsPerRow);
 }
 
-bottone.addEventListener('click', function(){
 
-    let grid = document.getElementById('grid');
+function generateGameGrid(cellsNumber, cellsPerRow){
     
-    grid.innerHTML = "";
+    document.querySelector('.container-griglia').inneHTML = '';
+    
+    const grid = document.createElement('div');
+    grid.classList.add('griglia');
+    
+    const grid_side = cellsPerRow * 70;
+    
+    grid.style.width = `${grid_side}px`
+    grid.style.height = `${grid_side}px`
 
-    for (let i = 0; i < 100; i++){
-           
-        const quadratoDinamico = creaQuadrato(i);
-
-        grid.appendChild(quadratoDinamico);
-
-        quadratoDinamico.addEventListener('click', function(){
-            this.classList.toggle('clicked');
-            console.log(this.innerText);
+    for (let i=0; i <cellsNumber; i++){
+        const cell = createSingleCell(i + 1, cellsPerRow);
+        cell.addEventListener('click', function(){
+            this.classList.toggle('clicked')
+            console.log(this.innerText)
         })
-                
+
+        grid.appendChild(cell)
     }
+    
+    document.querySelector('.container-griglia').appendChild(grid)
+}
+
+
+function createSingleCell(num, cells_per_row){
+    const cell = document.createElement('div');
+    cell.classList.add('quadrato');
+
+    let sideLength = '70px';
+
+    cell.style.width = sideLength;
+    cell.style.height = sideLength;
+
+    cell.innerText = ++num;
+
+    return cell;
+}
+
+document.getElementById('bottone-play').addEventListener('click', function(){
+    createNewGame();
+
+    
+})
+
+
+// function creaQuadrato(numero){
+
+//     const quadrato = document.createElement('div');
+
+//     quadrato.classList.add('quadrato');
+
+//     quadrato.innerText = ++numero;
+
+//     return quadrato; 
+// }
+
+// bottone.addEventListener('click', function(){
+
+//     createNewGame();
+
+//     let grid = document.getElementById('grid');
+    
+//     grid.innerHTML = "";
+
+//     for (let i = 0; i < 100; i++){
+           
+//         const quadratoDinamico = creaQuadrato(i);
+
+//         grid.appendChild(quadratoDinamico);
+
+//         quadratoDinamico.addEventListener('click', function(){
+//             this.classList.toggle('clicked');
+//             console.log(this.innerText);
+//         })
+                
+//     }
 
       
 
@@ -39,4 +117,4 @@ bottone.addEventListener('click', function(){
 
     
     
-})
+// })
